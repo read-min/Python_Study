b157ce1460b754bf29e0dcaf30e7dc2be62d518e
Kali-KM <rudals223@gmail.com>
2015-03-07 21:43:06 +0900

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+#setup.py
+#PY2EXE
+
+from distutils.core import setup
+import py2exe
+
+setup(console=['filename.py'],)
+
+#usage:
+#Set 'filename.py'
+#change the directory 
+#python setup.py py2exe
